2950a4d4edf2769b58b3d8a356f7b22790d9c3f6
dangoodie <dan.gooden.dev@gmail.com>
2023-04-14 16:56:19 +1000

    single row now displaying evenly based on the container

--- etchASketch.js
+++ etchASketch.js
@@ -1,1 +1,7 @@
 const gameContainer = document.querySelector("#game-container");
+
+for (let i = 0; i < 16; i++) {
+  const div = document.createElement('div');
+  gameContainer.appendChild(div);
+  
+}
