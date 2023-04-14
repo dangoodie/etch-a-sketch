1fb2b9b8f7cbf0657cb709346ce020d63c42ae98
dangoodie <dan.gooden.dev@gmail.com>
2023-04-14 23:08:57 +1000

    drawing now works with drag and click functionality

--- etchASketch.js
+++ etchASketch.js
@@ -1,4 +1,22 @@
 const gameContainer = document.querySelector("#game-container");
+document.addEventListener("mousedown", handleMouseDown);
+document.addEventListener("mouseup", handleMouseUp);
+let mouseDown = false;
+
+function handleMouseDown (e) {
+  if(e) mouseDown = true;
+}
+
+function handleMouseUp (e) {
+  if(e) mouseDown = false;
+}
+
+function paint(e) {
+
+  if (mouseDown) {
+    e.target.classList.add("black");
+  }
+}
 
 for (let i = 0; i < 16; i++) {
   const row = document.createElement("div");
@@ -7,15 +25,7 @@ for (let i = 0; i < 16; i++) {
   for (let i = 0; i < 16; i++) {
     const div = document.createElement("div");
     div.classList.add("cell");
+    div.addEventListener("mouseover", paint);
     row.appendChild(div);
   }
-  
 }
-// const row = document.createElement("div");
-// row.classList.add("row");
-// gameContainer.appendChild(row);
-// for (let i = 0; i < 16; i++) {
-//   const div = document.createElement("div");
-//   div.classList.add("cell");
-//   row.appendChild(div);
-// }
