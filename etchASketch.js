a18f8eb19a28d89b8421fad2059946cb7acc3975
dangoodie <dan.gooden.dev@gmail.com>
2023-04-14 17:20:01 +1000

    now with working rows and cells!

--- etchASketch.js
+++ etchASketch.js
@@ -1,7 +1,21 @@
 const gameContainer = document.querySelector("#game-container");
 
 for (let i = 0; i < 16; i++) {
-  const div = document.createElement('div');
-  gameContainer.appendChild(div);
+  const row = document.createElement("div");
+  row.classList.add("row");
+  gameContainer.appendChild(row);
+  for (let i = 0; i < 16; i++) {
+    const div = document.createElement("div");
+    div.classList.add("cell");
+    row.appendChild(div);
+  }
   
 }
+// const row = document.createElement("div");
+// row.classList.add("row");
+// gameContainer.appendChild(row);
+// for (let i = 0; i < 16; i++) {
+//   const div = document.createElement("div");
+//   div.classList.add("cell");
+//   row.appendChild(div);
+// }
